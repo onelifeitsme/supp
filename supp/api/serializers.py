6974c00492892b39f72857837b9daf8ad3a81f97
Artur Karpovich <onelifeitsme@gmail.com>
2022-02-06 03:21:25 +0300

    djoser_user_create_serializer_changed

--- supp/api/serializers.py
+++ supp/api/serializers.py
@@ -1,7 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions as django_exceptions
 from django.core.validators import validate_email
+from djoser.conf import settings
 from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
+from rest_framework.settings import api_settings
 from tickets.models import Message, Ticket
+
+
+User = get_user_model()
 
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -35,8 +43,25 @@ class ClientTicketSerializer(serializers.ModelSerializer):
 
 class MyUserCreateSerializer(UserCreateSerializer):
     """Кастомный сериализатор регистрации"""
+    email = serializers.EmailField(style={"input_type": "password"}, write_only=True)
 
-    def validate_email(self, email):
-        """Добавляем валидацию email, делая поле обязательным"""
-        validate_email(email)
-        return email
+    class Meta:
+        model = User
+        fields = tuple(User.REQUIRED_FIELDS) + (
+            settings.LOGIN_FIELD,
+            settings.USER_ID_FIELD,
+            "password",
+            'email',
+        )
+
+    def validate(self, attrs):
+        user = User(**attrs)
+        password = attrs.get("password")
+        email = attrs.get('email')
+        try:
+            validate_password(password, user)
+            validate_email(email)
+        except django_exceptions.ValidationError as e:
+            serializer_error = serializers.as_serializer_error(e)
+            raise serializers.ValidationError({"password": serializer_error[api_settings.NON_FIELD_ERRORS_KEY]})
+        return attrs
